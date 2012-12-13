3a8f9f083dd49d17951e8757ba549d5ced76c6ab
Johann Höchtl <johann.hoechtl@gmail.com>
2012-12-13 21:59:10 +0100

    new file: check OGD metadata consistency

---
+++ ogdat_check.go
@@ -0,0 +1,38 @@
+package ogdat
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+type ISO6392Lang struct {
+	Code, Identifier string
+}
+
+const iso639file = "ISO-639-2_utf-8.txt"
+
+var isolangfilemap map[string]*ISO6392Lang
+
+func init() {
+	reader, err := os.Open(iso639file)
+	if err == nil {
+		isolangfilemap = make(map[string]*ISO6392Lang)
+		csvreader := csv.NewReader(reader)
+		csvreader.Comma = '|'
+
+		for record, err := csvreader.Read(); err != io.EOF; record, err = csvreader.Read() {
+			isorecord := &ISO6392Lang{Code: record[0], Identifier: record[3]}
+			isolangfilemap[record[0]] = isorecord
+			if len(record[1]) > 0 {
+				isorecord = &ISO6392Lang{Code: record[1], Identifier: record[3]}
+				isolangfilemap[record[1]] = isorecord
+			}
+		}
+		log.Printf("Info: Read %d ISO language records", len(isolangfilemap))
+	} else {
+		log.Printf("Warning: Read %d ISO language records", len(isolangfilemap))
+	}
+
+}
