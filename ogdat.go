a20bfb18d2ed64b2ca81e261098d0cfbd1046fa1
Johann Höchtl <johann.hoechtl@gmail.com>
2012-12-15 21:03:40 +0100

    move description into separate file

--- ogdat.go
+++ ogdat.go
@@ -15,20 +15,6 @@ const (
 	OGDTimeUnknow
 )
 
-type Beschreibung struct {
-	ID            int
-	Bezeichner    string
-	OGD_Kurzname  string
-	CKAN_Feld     string
-	Defintion_DE  string
-	Erlauterung   string
-	Beispiel      string
-	ONA2270       string
-	ISO19115      string
-	RDFProperty   string
-	Definition_EN string
-}
-
 type Kategorie struct {
 	NumID       int `json:"-"`
 	ID          string
---
+++ ogdat_spec.go
@@ -0,0 +1,15 @@
+package ogdat
+
+type Beschreibung struct {
+	ID            int
+	Bezeichner    string
+	OGD_Kurzname  string
+	CKAN_Feld     string
+	Defintion_DE  string
+	Erlauterung   string
+	Beispiel      string
+	ONA2270       string
+	ISO19115      string
+	RDFProperty   string
+	Definition_EN string
+}
