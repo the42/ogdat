2e6f0603092dfabf43386507ff6973f6efa75c77
Johann Höchtl <johann.hoechtl@gmail.com>
2012-12-18 19:36:12 +0100

    rename test function to what it is actually testing

--- ogdatv21/ogdatv21_test.go
+++ ogdatv21/ogdatv21_test.go
@@ -2,15 +2,14 @@ package ogdatv21
 
 import "testing"
 
-func TestLoadISOFile(t *testing.T) {
+func TestLoadOGDATV21SpecFile(t *testing.T) {
 	if spec := loadogdatv21spec(ogdatv21specfile); spec != nil {
-		if spec != nil {
-			if nspec := len(spec); nspec != 31 {
-				t.Errorf("Should contain 31 records but found %d", nspec)
-			}
+		if nspec := len(spec); nspec != 31 {
+			t.Errorf("Should contain 31 records but found %d", nspec)
 		} else {
-			t.Log("OGDATV21 specification file not found, so reading not tested")
+			// t.Error("SUCCESS")
 		}
-
+	} else {
+	  t.Log("Specification file %s not loaded", ogdatv21specfile)
 	}
 }
