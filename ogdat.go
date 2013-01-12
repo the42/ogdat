4d3095ae3e60c1816af72ce06d0713d8c1e09d97
Johann Höchtl <johann.hoechtl@gmail.com>
2013-01-12 22:49:51 +0100

    separate check and miscellaneous functionality of ogdat core packages into different files:
    ogdat: Defintion (Beschreibung)
    ogdatcheck: check and utility functions

--- ogdat.go
+++ ogdat.go
@@ -1,17 +1,9 @@
 package ogdat
 
 import (
-	"encoding/csv"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
 	"reflect"
-	"regexp"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
 /*	Version10 = "OGD Austria Metadata 1.0" // Version 1.0: 24.10.2011
@@ -19,49 +11,6 @@ import (
 	Version20 = "OGD Austria Metadata 2.0" // Version 2.0: 10.10.2012
 	Version21 = "OGD Austria Metadata 2.1" // Version 2.1: 15.10.2012
 */
-
-const iso639file = "ISO-639-2_utf-8.txt"
-
-var isolangfilemap map[string]*ISO6392Lang = nil
-
-type ISO6392Lang struct {
-	Code, Identifier string
-}
-
-func CheckISOLanguage(lang string) bool {
-	if isolangfilemap == nil {
-		var err error
-		if isolangfilemap, err = loadisolanguagefile(iso639file); err != nil {
-			panic(fmt.Sprintf("Can not load ISO language file '%s'", iso639file))
-		}
-	}
-	_, ok := isolangfilemap[lang]
-	return ok
-}
-
-func loadisolanguagefile(filename string) (isolangfilemap map[string]*ISO6392Lang, _ error) {
-	reader, err := os.Open(iso639file)
-	if err != nil {
-		return nil, err
-	}
-
-	defer reader.Close()
-	isolangfilemap = make(map[string]*ISO6392Lang)
-	csvreader := csv.NewReader(reader)
-	csvreader.Comma = '|'
-
-	for record, err := csvreader.Read(); err != io.EOF; record, err = csvreader.Read() {
-		isorecord := &ISO6392Lang{Code: record[0], Identifier: record[3]}
-		isolangfilemap[isorecord.Code] = isorecord
-		if len(record[1]) > 0 {
-			isorecord = &ISO6392Lang{Code: record[1], Identifier: record[3]}
-			isolangfilemap[record[1]] = isorecord
-		}
-	}
-	log.Printf("Info: Read %d ISO language records", len(isolangfilemap))
-
-	return
-}
 
 var specification = make(map[string]*OGDSet)
 
@@ -88,99 +37,6 @@ type Beschreibung struct {
 	Definition_EN string
 	occurrence    Occurrence
 	version       string
-}
-
-type CheckMessage struct {
-	Type    int // 1 = Info, 2 = Warning, 3 = Error
-	Text    string
-	OGDID   int
-	Context string
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-type CheckError struct {
-	Status, Position int
-	message          string
-}
-
-func (ce *CheckError) Error() string {
-	return ce.message
-}
-
-var regexphtmlcodecheck = regexp.MustCompile(`<\w+.*('|"|)>`)
-var regexphtmlescape = regexp.MustCompile(`&\w{1,10};|&#\d{1,6};`)
-var regexpurlencode = regexp.MustCompile(`%[0-9a-fA-F][0-9a-fA-F]`)
-var regexpposixescape = regexp.MustCompile(`\\n|\\b|\\v|\\t`)
-
-// return values are:
-// ok = false indicates sthg. was wrong in which case error will not be nil
-//
-// error: if is of type CheckError:
-// Status: 1 = Info, 2 = Warning, 3 = Error
-// Position: beginning position of offending input
-// message: An error message describing the problem
-func CheckOGDTextStringForSaneCharacters(str string) (ok bool, _ error) {
-	if !utf8.ValidString(str) {
-		return false, &CheckError{3, -1, "Zeichenfolge ist nicht durchgängig gültig als UTF8 kodiert"}
-	}
-	if idx := regexphtmlcodecheck.FindIndex([]byte(str)); idx != nil {
-		return false, &CheckError{2, idx[0], fmt.Sprintf("Mögliche HTML-Sequenz: '%s'", str[idx[0]:min(20, idx[1]-idx[0])])}
-	}
-	if idx := regexphtmlescape.FindIndex([]byte(str)); idx != nil {
-		return false, &CheckError{2, idx[0], fmt.Sprintf("Mögliche HTML-Escapes: '%s'", str[idx[0]:min(15, idx[1]-idx[0])])}
-	}
-	if idx := regexpurlencode.FindIndex([]byte(str)); idx != nil {
-		return false, &CheckError{2, idx[0], fmt.Sprintf("Mögliche Url-Escapes: '%s'", str[idx[0]:min(8, idx[1]-idx[0])])}
-	}
-	if idx := regexpposixescape.FindIndex([]byte(str)); idx != nil {
-		return false, &CheckError{2, idx[0], fmt.Sprintf("Mögliche Posix-Escapes: '%s'", str[idx[0]:min(5, idx[1]-idx[0])])}
-	}
-	return true, nil
-}
-
-var regexpbboxWKT = regexp.MustCompile(`^POLYGON\s{0,1}\({1,2}\s{0,2}[-+]?[0-9]*\.?[0-9]+\s{1,2}[-+]?[0-9]*\.?[0-9]+,\s{0,2}[-+]?[0-9]*\.?[0-9]+\s{1,2}[-+]?[0-9]*\.?[0-9]+\s{0,2}\){1,2}$`)
-
-func CheckOGDBBox(str string) (bool, error) {
-	if !utf8.ValidString(str) {
-		return false, &CheckError{3, -1, "Zeichenfolge ist nicht durchgängig gültig als UTF8 kodiert"}
-	}
-	if idx := regexpbboxWKT.FindIndex([]byte(str)); idx == nil {
-		return false, &CheckError{3, -1, "Keine gültige WKT-Angabe einer BoundingBox"}
-	}
-	return true, nil
-}
-
-var regexpEMail = regexp.MustCompile(`^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$`)
-
-func CheckUrlContact(url string, followhttplink bool) (bool, error) {
-	// it's a contact point if it's a http-link (starts with "http(s)" )
-	if len(url) >= 4 && url[:4] == "http" || len(url) >= 5 && url[:5] == "https" {
-		if followhttplink {
-			resp, err := http.Head(url)
-			if err != nil {
-				return false, &CheckError{3, -1, fmt.Sprintf("URL kann nicht aufgelöst werden: '%s'", err)}
-			}
-			if sc := resp.StatusCode; sc != 200 {
-				return false, &CheckError{3, -1, fmt.Sprintf("HEAD request liefert nicht-OK Statuscode '%d'", sc)}
-			}
-		}
-		return true, nil
-	}
-	// it's a contact point if it's an email address
-	if idx := regexpEMail.FindIndex([]byte(url)); idx != nil {
-		return true, nil
-	}
-	return false, &CheckError{2, -1, fmt.Sprintf("vermutlich keine gültige Web- oder E-Mail Adresse: '%s' (Auszug)", url[:min(20, len(url))])}
-}
-
-type Checker interface {
-	Check(bool) []CheckMessage
 }
 
 func NewBeschreibung(ID int, occur Occurrence, ver string) *Beschreibung {
@@ -251,55 +107,4 @@ func MetaDataStructFieldIsNil(val interface{}) bool {
 		panic("Struct field is not a pointer")
 	}
 	return v.IsNil()
-}
-
-func Loadogdatspec(version, filename string) (*OGDSet, error) {
-	reader, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer reader.Close()
-
-	csvreader := csv.NewReader(reader)
-	csvreader.Comma = '|'
-	csvreader.LazyQuotes = true
-
-	// Read the first line and use it as the labels for the items to load
-	record, err := csvreader.Read()
-	if err != nil {
-		return nil, err
-	}
-
-	set := &OGDSet{Label: record}
-
-	spec := make([]*Beschreibung, 0)
-	for record, err = csvreader.Read(); err != io.EOF; record, err = csvreader.Read() {
-		id, _ := strconv.Atoi(record[0])
-		var occ Occurrence
-		switch record[12][0] {
-		case 'R':
-			occ = OccRequired
-		case 'O':
-			occ = OccOptional
-		}
-		descrecord := NewBeschreibung(id, occ, version)
-
-		descrecord.Bezeichner = record[1]
-		descrecord.OGD_Kurzname = record[2]
-		descrecord.CKAN_Feld = record[3]
-		descrecord.Anzahl = record[4]
-		descrecord.Definition_DE = record[5]
-		descrecord.Erlauterung = record[6]
-		descrecord.Beispiel = record[7]
-		descrecord.ONA2270 = record[8]
-		descrecord.ISO19115 = record[9]
-		descrecord.RDFProperty = record[10]
-		descrecord.Definition_EN = record[11]
-
-		spec = append(spec, descrecord)
-	}
-	set.Beschreibung = spec
-	log.Printf("Info: Read %d %s specifiaction records", len(spec), version)
-
-	return set, nil
 }
