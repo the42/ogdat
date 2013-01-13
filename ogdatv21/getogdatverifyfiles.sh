3f682a03eb2d577bf8b178c091392901e89948db
Johann Höchtl <johann.hoechtl@gmail.com>
2013-01-13 10:39:10 +0100

    make bash script to retrieve check utitlity files more robust

--- ogdatv21/getogdatverifyfiles.sh
+++ ogdatv21/getogdatverifyfiles.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 OGDV21SPEC=http://www.ref.gv.at/uploads/media/OGD-Metadaten_2_1_2012_10.pdf
 ISO639LANGFILE=http://www.loc.gov/standards/iso639-2/ISO-639-2_utf-8.txt
 IANACHARSET=http://www.iana.org/assignments/character-sets/character-sets.xml
 
 # retrieving specification from the internet
 echo "Beziehen der Spezifikation"
+rm `basename  $OGDV21SPEC`.*  || true
+rm `basename  $OGDV21SPEC` || true
 wget $OGDV21SPEC
 # remove supperfluous pdfs from repeated retrieval (need no history)
-rm `basename  $OGDV21SPEC`.*
+
 
 PDF=`basename  $OGDV21SPEC`
 TXT=`basename  $OGDV21SPEC .pdf`.txt
@@ -21,15 +25,18 @@ echo "Extrahieren der Information als CSV ..."
 awk -f convogdatspectocsv.awk $TXT > $CSV
 
 echo "Beziehen der ISO Sprachdatei"
+rm `basename  $ISO639LANGFILE`.* || true
+rm `basename  $ISO639LANGFILE` || true
 wget $ISO639LANGFILE
-rm `basename  $ISO639LANGFILE`.*
 
 echo "Beziehen der IANA Charset-Datei"
+rm `basename  $IANACHARSET`.* || true
+rm `basename  $IANACHARSET` || true
 wget $IANACHARSET
-rm `basename  $IANACHARSET`.*
 
-CSV=`basename  $IANACHARSET .xml`.csv
+XML=`basename  $IANACHARSET`
+TXT=`basename  $IANACHARSET .xml`.txt
 echo "Extrahieren der Information als CSV ..."
-xsltproc character-sets.xsl character-sets.xml > character-sets.csv
+xsltproc character-sets.xsl $XML > $TXT
 
 echo "Erfolgreich"
