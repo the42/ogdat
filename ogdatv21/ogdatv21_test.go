2e8cea10e0a998469104f286138c80e4a169cdca
Johann Höchtl <johann.hoechtl@gmail.com>
2013-02-09 23:44:00 +0100

    read tests as JSON-stream from file

--- ogdatv21/ogdatv21_test.go
+++ ogdatv21/ogdatv21_test.go
@@ -1,1 +1,55 @@
 package ogdatv21
+
+import (
+	"encoding/json"
+	"github.com/the42/ogdat"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+type checkRequest struct {
+	filename    string
+	followlinks bool
+}
+
+type checkResponse struct {
+	message []ogdat.CheckMessage
+	err     error
+}
+
+type checkTest struct {
+	in  *checkRequest
+	out *checkResponse
+}
+
+var checkTests = []checkTest{
+	{
+		&checkRequest{"file1_test.json", false},
+		nil,
+	},
+}
+
+func TestCheck(t *testing.T) {
+	md := &MetaData{}
+
+	for _, val := range checkTests {
+		file, err := os.Open(val.in.filename)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ogdjsonmd, err := ioutil.ReadAll(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := json.Unmarshal(ogdjsonmd, md); err != nil {
+			t.Fatalf("Can't unmarshall byte stream: %s\n", err)
+		}
+		res, err := md.Check(val.in.followlinks)
+		if val.out != nil {
+			// TODO: compare results
+			_ = res
+		}
+		file.Close()
+	}
+}
