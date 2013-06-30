913d6b65a0c17ecd4bec11538302fbbb46828aeb
Johann Höchtl <johann.hoechtl@gmail.com>
2013-06-30 20:49:11 +0200

    no need for a closure, cf. https://github.com/emicklei/go-restful/issues/21#issuecomment-20252820

--- analyser/web.go
+++ analyser/web.go
@@ -6,62 +6,59 @@ import (
 	"strconv"
 )
 
-func (an *analyser) GetRESTEntities() func(request *restful.Request, response *restful.Response) {
-	return func(request *restful.Request, response *restful.Response) {
-		id := request.PathParameter("id")
-		sortorder := request.QueryParameter("sortorder")
-		numds := request.QueryParameter("numds")
+func (an *analyser) GetRESTEntities(request *restful.Request, response *restful.Response) {
 
-		var entity, command string
-		nums := -1
-		returnnums := false
+	getentity := request.PathParameter("entity")
+	_ = getentity
+	sortorder := request.QueryParameter("sortorder")
+	numds := request.QueryParameter("numds")
 
-		if sortorder == "asc" {
-			command = "ZRANGE"
-		} else {
-			command = "ZREVRANGE"
-		}
+	var entity, command string
+	nums := -1
+	returnnums := false
 
-		if len(numds) > 0 {
-			var err error
-			returnnums, err = strconv.ParseBool(numds)
-			if err != nil {
-				returnnums = false
-			}
-		}
+	if sortorder == "asc" {
+		command = "ZRANGE"
+	} else {
+		command = "ZREVRANGE"
+	}
 
-		var reply []interface{}
+	if len(numds) > 0 {
 		var err error
+		returnnums, err = strconv.ParseBool(numds)
+		if err != nil {
+			returnnums = false
+		}
+	}
+
+	var reply []interface{}
+	var err error
+
+	if returnnums {
+		reply, err = redis.Values(an.rcon.Do(command, "entities", 0, -1, "WITHSCORES"))
+	} else {
+		reply, err = redis.Values(an.rcon.Do(command, "entities", 0, -1))
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	resultset := make([]UnitDSNums, 0)
+
+	for len(reply) > 0 {
 		if returnnums {
-			reply, err = redis.Values(an.rcon.Do(command, "entities", 0, -1, "WITHSCORES"))
+			reply, err = redis.Scan(reply, &entity, &nums)
 		} else {
-			reply, err = redis.Values(an.rcon.Do(command, "entities", 0, -1))
+			reply, err = redis.Scan(reply, &entity)
+
 		}
 		if err != nil {
 			panic(err)
 		}
-
-		resultset := make([]UnitDSNums, 0)
-
-		for len(reply) > 0 {
-			a := UnitDSNums{}
-			if returnnums {
-				reply, err = redis.Scan(reply, &entity, &nums)
-			} else {
-				reply, err = redis.Scan(reply, &entity)
-
-			}
-			a.Numsets = nums
-			a.Entity = entity
-			if err != nil {
-				panic(err)
-			}
-			resultset = append(resultset, a)
-		}
-
-		println("Hallo Welt", id)
-		response.WriteEntity(resultset)
+		resultset = append(resultset, UnitDSNums{Entity: entity, Numsets: nums})
 	}
+
+	response.WriteEntity(resultset)
 }
 
 func NewAnalyseOGDATRESTService(an *analyser) *restful.WebService {
@@ -70,7 +67,7 @@ func NewAnalyseOGDATRESTService(an *analyser) *restful.WebService {
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
-	ws.Route(ws.GET("/entities/{id}").To(an.GetRESTEntities()).
+	ws.Route(ws.GET("/entities/{entity}").To(an.GetRESTEntities).
 		// for documentation
 		Doc("Get entities within the Database").
 		Param(ws.PathParameter("id", "the identifier of the entity to return. Leave empty for all")).
